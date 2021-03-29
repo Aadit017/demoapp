719c36113b74bbad45937c6eddd7283272b57fb0
Aadit <baggaaadit017@gmail.com>
2021-03-29 20:16:51 +0530

    creating button component

---
+++ src/components/Button/Button.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import "./Button.style.css"
+import { Link } from 'react-router-dom';
+
+const STYLES=['btn--primary','btn--outline']
+const SIZES =['btn--medium','btn--large']
+
+const Button = ({children,type,onClick,buttonStyle,buttonSize}) => {
+    const checkButtonStyle=STYLES.includes(buttonStyle) ? 
+    buttonStyle:STYLES[0]
+    const checkButtonSizes=SIZES.includes(buttonSize)?buttonSize:SIZES[0]
+    return (
+        <div>
+            <Link to="sign-up" class="btn-mobile">
+                <button>
+                    {children} 
+                </button>
+            </Link>            
+        </div>
+    )
+}
+
+// {children} is just for the text to be written in the button 
+export default Button
--- src/components/navbar/Navbar.component.jsx
+++ src/components/navbar/Navbar.component.jsx
@@ -1,11 +1,14 @@
 import React,{useState} from 'react'
 import { Link} from 'react-router-dom';
-import FastfoodIcon from '@material-ui/icons/Fastfood';
 import MenuIcon from '@material-ui/icons/Menu';
+import CloseIcon from '@material-ui/icons/Close';
 const Navbar = () => {
     const [click,setClick]=useState(false)
     const handleClick=()=>{
         setClick(!click)
+    }
+    const closeMobileMenu=()=>{
+        setClick(false)
     }
     return (
         <>
@@ -13,11 +16,26 @@ const Navbar = () => {
         <nav className="navbar">
             <div className="navbar-container">
                 <Link to="/" className="navbar-logo">ASB</Link>
-            <div className="menu-icons" onClick={handleClick}>
+            <div className="menu-icons " onClick={handleClick}>
                {
-                   click ? (<FastfoodIcon />) : (<MenuIcon />)
+                   click ? (<CloseIcon />) : (<MenuIcon />)
                }
             </div>
+            <ul className={click?'nav-menu active ' :'nav-menu'}>
+                <li className="menu-item">
+                    <Link to="/" className="nav-links" onClick={closeMobileMenu}>Home</Link>
+                </li>
+                <li className="menu-item">
+                    <Link to="/services" className="nav-links" onClick={closeMobileMenu}>Services</Link>
+                </li>
+                <li className="menu-item">
+                    <Link to="/products" className="nav-links" onClick={closeMobileMenu}>Products</Link>
+                </li>
+                <li className="menu-item">
+                    <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>Sign Up</Link>
+                </li>
+            </ul>
+            
             </div>
         </nav>
 
